70421042a36846126a965fab9d67de29257bdd86
ortal <ortalazylay96@gmail.com>
2023-06-01 08:16:33 +0300

    m

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,6 +1,5 @@
-import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ChartOptions } from 'chart.js';
-import { Todos } from 'src/app/models/todos';
 
 @Component({
   selector: 'app-pie-chart',
@@ -9,29 +8,21 @@ import { Todos } from 'src/app/models/todos';
 })
 export class PieChartComponent implements OnInit, OnChanges{
 
-
-
-  // @Input() todosArr: any;
-  // completed: number = 0;
-  // notCompleted: number = 0;
-
   @Input() completedTodo: any;
   @Input() notCompletedTodo: any;
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.banan();
+    this.initPie();
   }
 
   ngOnInit(): void {
   }
 
-  public banan() {
+  public initPie() {
     this.pieChartDatasets = [{
       data: [this.completedTodo, this.notCompletedTodo]
     }]
     this.pieChartLabels = ['completed', 'not completed'];
-    console.log(this.completedTodo)
-    console.log(this.notCompletedTodo)
   }
 
 
